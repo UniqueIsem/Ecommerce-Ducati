d2c1dda8d50863e2085b8ebfac34d521a5464570
Isaac Santos <isaac.emanuel@live.com>
2024-05-13 03:47:30 -0600

    improve hp design

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,15 +6,20 @@ export const HomePage = () => {
             <div className='hp-blank-space'></div>
             <div className='hp-info-container'>
                 <div className='hp-text-container'>
-                <div className='hp-message'>
-                    <div className='hp-title'>
-                        <h1>WELCOME TO DUCATI STORE</h1>
+                    <div className='hp-message'>
+                        <div className='hp-title'>
+                            <h1>WELCOME TO DUCATI </h1>
+                        </div>
+                        <p className='hp-subtitle'>
+                            Dive into the universe of emotions and power with Ducati. From the legendary Panigale to the versatile Multistrada, 
+                            each motorcycle is an expression of our commitment to excellence and innovation. Join a passionate community of enthusiasts 
+                            and experience the thrill of the road like never before. Discover the style, elegance, and performance that only Ducati 
+                            can offer. Welcome to the world of two wheels, welcome to Ducati.
+                        </p>
                     </div>
-                    <p>in this store we'll intoduce you to the gratest machines over the world</p>
-                </div>
                 </div>
                 <div className='hp-button-container'>
-                    <button className='hp-button'>SHOP NOW</button>
+                    <button className='hp-button'>Shop Now</button>
                 </div>
             </div>
         </div>
