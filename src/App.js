cfe74b54a5e5575d715c24a2b7d8e613eea6d63c
Isaac Santos <isaac.emanuel@live.com>
2024-05-13 01:21:36 -0600

    +header design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Header></Header>
       </header>
+      <main className=''>
+
+      </main>
       <body className="App-body">
 
       </body>
